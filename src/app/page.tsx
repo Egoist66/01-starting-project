136bb8a5f528d079caca3ecefddb2152edff9d5d
Farid Mahmudov <Razormad666@gmail.com>
2025-01-17 17:24:06 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch('https://tentfull.by')
+  const html = await data.text()
+  console.log(html);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div  className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -12,6 +16,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
